9d958c4c5ecf607ee841930cc0e1998d356b7880
JoliopoLi <l_henryz@live.concordia.ca>
2023-02-26 19:55:36 -0500

    signin and signup
    Created a signin and signup templates
    
    integrated postsignin and postsignup functions into the project
    
    user can now create account and then signin which will direct them back to the home page

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,14 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import cringe, redirect_signin
+from .views import home, redirect_signin, redirect_signup, postsignup, postsignin
 
 urlpatterns = [
-    path('', views.cringe, name='homepage'),
-    path('redirect/', views.redirect_signin, name='signin'),   
+    path('', views.home, name='homepage'),
+    path('signin/', views.redirect_signin, name='signin'), 
+    path('signup/', views.redirect_signup, name='signup'),
+    path('admin/', admin.site.urls), 
+    path('postsignup/', views.postsignup, name='postsignup'),
+    path('postsignin/', views.postsignin, name='postsignin')
 ]
+
