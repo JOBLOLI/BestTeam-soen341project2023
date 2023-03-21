07b759e8227abb35f3cd9a53a5cb345e3d230fce
mikeyd234 <mikey_d234@hotmail.com>
2023-03-21 14:25:23 -0400

    added function for creating a user profile

--- main/views.py
+++ main/views.py
@@ -16,6 +16,7 @@ config={
 firebase=pyrebase.initialize_app(config)
 auth=firebase.auth()
 database=firebase.database()
+storage = firebase.storage()
 
 def home(request):
     context = {'foo': 'bar'}
@@ -29,6 +30,9 @@ def redirect_signup(request):
 
 def redirect_delete(request):
     return render(request, 'delete.html')
+
+def redirect_profile(request):
+    return render(request, 'profilecreate.html')
 
 # signin and signup options
 def postsignup(request):
@@ -58,7 +62,73 @@ def postsignin(request):
 
 
 def accountDeletion(request):
-    """ Deletes account of currently logged in user"""
+    """Deletes account of currently logged in user"""
     user = auth.current_user['localId']
     auth.delete_user_account(user['idToken'])
     return render(request, 'home.html')
+
+def postProfile(request):
+    """Allows user to customize their profile"""
+
+     #Basic Information
+
+    name = request.POST.get('name')
+    title = request.POST.get('title')
+    experience = request.POST.get('experience')
+    academics = request.POST.get('academics')
+    gender = request.POST.get('gender')
+    age = request.POST.get('age')
+    url = request.POST.get('url') #URL for profile photo
+
+    dataBasic = {'Name': name, 'Title': title, 'Exeperience': experience, 'Academics': academics, 'Gender': gender, 'Age': age, 'PhotoURL': url}
+
+    database.child('users').child('profiles').child('basicInfo').push(dataBasic)
+
+    # Contact information
+
+    phone = request.POST.get('phone')
+    address = request.POST.get('address')
+    email = request.POST.get('email')
+    website = request.POST.get('website')
+
+    dataContact = {'Phone': phone, 'Address': address, 'Email': email, 'Website': website}
+
+    database.child('users').child('profiles').child('contactInfo').push(dataContact)
+
+    #Education
+
+    gradDate = request.POST.get('gradDate')
+    degree = request.POST.get('degree')
+    school = request.POST.get('school')
+
+    dataEducation = {'Date': gradDate, 'Degree': degree, 'School': school}
+
+    database.child('users').child('profiles').child('education').push(dataEducation)
+
+    #Experience
+
+    exp1Date = request.POST.get('exp1Date')
+    exp2Date = request.POST.get('exp2Date')
+    exp3Date = request.POST.get('exp3Date')
+
+    exp1Title = request.POST.get('exp1Title')
+    exp2Title = request.POST.get('exp2Title')
+    exp3Title = request.POST.get('exp3Title')
+
+    exp1Company = request.POST.get('exp1Company')
+    exp2Company = request.POST.get('exp2Company')
+    exp3Company = request.POST.get('exp3Company')
+
+    dataExpDates = {'Date1': exp1Date, 'Date2': exp2Date, 'Date3': exp3Date}
+    dataExpTitles = {'Title1': exp1Title, 'Title2': exp2Title, 'Title3': exp3Title}
+    dataExpCompany = {'Company1': exp1Company, 'Company2': exp2Company, 'Company3': exp3Company}
+
+    database.child('users').child('profiles').child('experience').child('dates').push(dataExpDates)
+    database.child('users').child('profiles').child('experience').child('titles').push(dataExpTitles)
+    database.child('users').child('profiles').child('experience').child('company').push(dataExpCompany)
+
+    #Profile Photo
+    
+
+
+    return render(request,'profile.html')
