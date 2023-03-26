d3bbf98944c4e1e83bcd5b2d39f418cc3666af99
JoliopoLi <l_henryz@live.concordia.ca>
2023-03-25 20:20:30 -0400

    view profile based on acc signed in
    can view account information in profile page

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import home, redirect_signin, redirect_signup, postsignup, postsignin, redirect_job_creation, create_job, redirect_admin, jobview, logout, applytojob, redirect_profile
+from .views import home, redirect_signin, redirect_signup, postsignup, postsignin, redirect_job_creation, create_job, redirect_admin, jobview, profileview, logout, applytojob, redirect_profile
 
 urlpatterns = [
     path('', views.home, name='homepage'),
@@ -16,5 +16,6 @@ urlpatterns = [
     path('jobview/', views.jobview, name='jobview'),
     path('applytojob/', views.applytojob, name='applytojob'),
     path('adminpage/', views.redirect_admin, name='adminpage'),
-    path('profile/', views.redirect_profile, name='profile')
+    path('profile/', views.redirect_profile, name='profile'),
+    path('profileview/', views.profileview, name='profileview')
 ]
